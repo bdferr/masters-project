24065c3190bc67ec1a1362290efd87e32f3a25aa
bdferr <skepticism9@gmail.com>
2018-01-11 14:18:16 -0500

    Update gs_concat2.py
    I made the same changes I made to gs_concat1.py: the same explanatory notes and the same deletion of superfluous variable initialization.

--- gs_concat2.py
+++ gs_concat2.py
@@ -1,20 +1,21 @@
+##This file concatenates all of the tagged and processed files from the second half of the corpus
+##into a single file, then does the same in a second file for the untagged and processed files.
 import os
 import re
 ##here I access all of the file names in the already processed directory for the second half:
 mydir='C:/users/brendan/documents/library and information science/masters_project/gold-standard/second_half_tagged/processed'
 secondhalf=next(os.walk(mydir))[2]
 allt = ''
-allu = ''
 s = ''
-u = ''
 if 'secondhalfgstagged.txt' in secondhalf:
     secondhalf.remove('secondhalfgstagged.txt')
+##here I add the contents of each tagged file in the second half to a single variable:
 for ofilename in secondhalf:
     filename = mydir + "/" + ofilename
     s = open(filename, 'r').read()
     print("processing %s \n" % filename)
     allt += s
-##here I write every tagged file from that half to a single file:
+##here I write the value of the variable to a single file:
 endpath = mydir + "/secondhalfgstagged.txt"
 with open("%s" % endpath,'w') as f:
     f.write(allt)
@@ -23,18 +24,17 @@ with open("%s" % endpath,'w') as f:
 
 mydir='C:/users/brendan/documents/library and information science/masters_project/gold-standard/second_half_untagged/processed'
 secondhalf=next(os.walk(mydir))[2]
-allt = ''
 allu = ''
-s = ''
-u = ''
+u = '' 
 if 'secondhalfgsuntagged.txt' in secondhalf:
     secondhalf.remove('secondhalfgsuntagged.txt')
+##here I add the contents of each untagged file in the second half to a single variable:
 for ofilename in secondhalf:
     filename = mydir + "/" + ofilename
     u = open(filename, 'r').read()
     print("processing %s \n" % filename)
     allu += u
-##here I write every untagged file from that half to a single file:
+##here I write the value of the variable to a single file:
 endpath = mydir + "/secondhalfgsuntagged.txt"
 with open("%s" % endpath,'w') as f:
     f.write(allu)
