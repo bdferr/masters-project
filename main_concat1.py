0231ef345eced671ce0256c246a7fcaaf8402c94
bdferr <skepticism9@gmail.com>
2018-01-11 17:59:04 -0500

    Update main_concat1.py
    I added more explanatory notes.

--- main_concat1.py
+++ main_concat1.py
@@ -1,12 +1,14 @@
 import os
 import re
-##here I access all of the file names in the already processed directory for the first half:
+##here I access all of the file names in the already processed directory for the first half of the non-gold-standard corpus:
 mydir='C:/users/brendan/documents/library and information science/masters_project/germanc-corpus/first_half_col/processed'
 firsthalf=next(os.walk(mydir))[2]
 allt = ''
 allu = ''
 s = ''
 u = ''
+##if a file name ends in "untagged.txt" its contents are added to one variable called u,
+##and if not it is added to another variable called s:
 for ofilename in firsthalf:
     if ofilename!='firsthalfgstagged.txt' and ofilename!="firsthalfuntagged.txt":
         if re.search("^.*untagged.txt$", ofilename):
@@ -19,13 +21,13 @@ for ofilename in firsthalf:
             s = open(filename, 'r').read()
             print("processing %s \n" % filename)
             allt += s
-##here I write tagged every file from that half to a single file:
+##here I write tagged every file from the first half half to a single file:
 endpath = mydir + "/firsthalftagged.txt"
 with open(endpath,'w') as f:
     f.write(allt)
     print("Entire tagged contents of folder written to file %s" % endpath)
     f.close()
-##here I write untagged every file from that half to a single file:
+##here I write untagged every file from the first half to a single file:
 endpath = mydir + "/firsthalfuntagged.txt"
 with open(endpath,'w') as f:
     f.write(allu)
