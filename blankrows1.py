7b113078ddb4b68725334b012b7f65d669eeee3f
bdferr <skepticism9@gmail.com>
2018-01-11 11:11:39 -0500

    Update blankrows1.py
    I am altering the comments in these files to make it clearer what these files do and why. I also removed some superfluous lines which I had commented out and only used for testing purposes when working on these files in 2015. Finally I fixed a mistake in a print command which displayed the path to each file twice (concatenated).

--- blankrows1.py
+++ blankrows1.py
@@ -1,19 +1,19 @@
+##In this script I check every file in the first half of the corpus, untagged,
+##to add a blank row after the end of every sentence.
 import os
 import re
-##here I access all of the file names in the directory for the first half:
-##os.path.isdir(mydirname),os.path.isfile(myfilename),os.path.exists(mysomethingname)
+##here I access all of the file names in the directory for the first half of the corpus, untagged:
 mydir='C:/users/brendan/documents/library and information science/masters_project/gold standard/first_half_untagged'
 firsthalfgs=next(os.walk(mydir))[2]
 
-##here I check to see if there is a $. token, indicating the end of a sentence, and add a blank row after it:
 all = ''
 for ofilename in firsthalfgs:
     s= ''
     if ofilename!='firsthalfgsuntagged.txt':
-        ##print(os.getcwd())
         filename = 'C:/users/brendan/documents/library and information science/masters_project/gold standard/first_half_untagged/' + ofilename
         print("processing %s \n" % filename)
-        ##Here I check to see if each line consists of . plus a newline character, and if so add another newline after it:
+        ##Here I check to see if each line consists of a period, exclamation point or question mark
+        ##plus a space character and a newline character, and if so add another newline after it:
         with open(filename,'r') as f:
             for token in f:
                 if re.search("^[.!?]\s\n$", token):
@@ -23,13 +23,12 @@ for ofilename in firsthalfgs:
                     s = s + token
         ##here I write each altered file to its own new file:
         newfilename = 'C:/users/brendan/documents/library and information science/masters_project/gold standard/first_half_untagged/processed/' + ofilename.split(".")[0] + "_untagged.txt"
-        ##print ("New file named ", newfilename)
         with open("%s" % newfilename,'w') as nf:
             nf.write(s)
-        print("File written to C:/users/brendan/documents/library and information science/masters_project/gold standard/first_half_untagged/processed/%s" % newfilename)
+        print("File written to %s" % newfilename)
         nf.close()
         all = all + s
-##here I write every file from that half to a single file:
+##here I write every file from the first half to a single file:
 with open("C:/users/brendan/documents/library and information science/masters_project/gold standard/first_half_untagged/firsthalfgsuntagged.txt",'w') as f:
     f.write(all)
     print("Entire contents of folder written to file C:/users/brendan/documents/library and information science/masters_project/gold standard/first_half_untagged/firsthalfuntagged.txt")
