4ff011ab7e10b55ef1bad778eccede5eacd2a105
bdferr <skepticism9@gmail.com>
2018-01-11 12:53:51 -0500

    Update catg-script1.py
    I added explanatory comments. I may need to come back to this because some of it still does not make sense to me.

--- catg-script1.py
+++ catg-script1.py
@@ -1,11 +1,14 @@
+##This script deals with the results of the tagging. It takes the full list of part of speech tags
+##in the tagged files, which is too long to be manageable, and puts them
+##into broader categories like "noun." It provides counts for these broader categories,
+##which are the only ones I used in displaying the results in tables.
 import os
 import re
-##there is nothing currently in this folder:
+##Here I get the file names for every file in the directory for documents which have been tagged.
 dirpath = 'C:/users/brendan/documents/library and information science/masters_project/germanc-corpus/tnt-tagged-rows'
 files = os.walk(dirpath)[2]
 
-##are the one-item lists really necessary? At least they remind me that those categories
-##really do contain only one item.
+##This uses lists to sort the initial part of speech tags into the broader categories I need.
 N = [NN, NE]
 V = [VAFIN, VAIMP, VVFIN, VVIMP, VMFIN, VVINF, VAINF, VMINF, VVIZU, VVPP, VMPP, VAPP]
 ART =[ART]
@@ -20,6 +23,8 @@ PTK = [PTKZU, PTKNEG, PTKVZ, PTKA, PTKANT]
 S = [ITJ, TRUNC, XY, FM]
 
 catlist = [N, V, ART, ADJ, P, CARD, ADV, KO, AP, I, PTK, S]
+##These names are needed for the print statements later in this script
+##which will inform me how many tokens of which type are found in each file:
 catnames = ["N", "V", "ART", "ADJ", "P", "CARD", "ADV", "KO", "AP", "I", "PTK", "S"]
 
 Ncount = 0
@@ -34,7 +39,7 @@ APcount = 0
 Icount = 0
 PTKcount = 0
 Scount = 0
-##can I keep a list of items which are actually variables like this?
+##This is a list of counts of parts of speech:
 countlist = [Ncount, Vcount, ARTcount, ADJcount, Pcount, CARDcount, ADVcount, KOcount, APcount, Icount, PTKcount, Scount]
 
 for file in files:
@@ -44,9 +49,10 @@ for file in files:
 	    for row in openedfile:
 		    i = 0
 		    for cat in catlist:
-			    ##increment a count for this list by one somehow
+	                    ##For each row in each file, if the second element of the row, stripped of whitespace, 
+	                    ##is an element in a category list, increment the count of that category.
 			    if row[1].strip() in cat:
 				    countlist[i] += 1
-				print countlist [i] "tokens of type" catnames[i] "found in file" file
+				print countlist[i] "tokens of type" catnames[i] "found in file" file
 			    i += 1
         rownum += 1
