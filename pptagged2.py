335acb4caa24e0b2df66d57ead15a862e74a7775
bdferr <skepticism9@gmail.com>
2018-01-12 15:40:14 +0000

    Update pptagged2.py
    I made very similar changes to those I made to pptagged1.py. I added explanatory notes, took out commented-out lines which I had used for testing, and fixed a line which excluded already concatenated files so that it referred to both of them, not just one.

--- pptagged2.py
+++ pptagged2.py
@@ -1,43 +1,52 @@
+#I am not sure I actually used this script in the end. In any case it deals with the second half
+#of the non-gold-standard tagged corpus. It is meant to produce a tagged and untagged version
+#of each file for further processing by stripping everything but the tokens and tags for the tagged version
+#and everything but the token for the untagged version.
 import os
 import re
-##here I access all of the file names in the directory for the second half:
+#Here I access all of the file names in the directory for the second half:
 mydir='C:/users/brendan/documents/library and information science/masters_project/germanc-corpus/second_half_col'
 secondhalf=next(os.walk(mydir))[2]
 allt = allu = ''
-##here I strip everything but the tokens and tags:
+#Here I initialize variables and access every row from every file:
 for ofilename in secondhalf:
     s= ''
     u = ''
-    if ofilename!='secondhalftagged.txt':
+    #I am leaving this in here to show my thinking at the time;
+    #if I had put the results of this script into the same folder and run it multiple times without this,
+    #I would have gotten results that got larger and more full of duplicated text each time,
+    #but the already concatenated files should be in a separate folder
+    #according to this script, so this line to exclude them should not be necessary:
+    if ofilename!='secondhalftagged.txt' and ofilename != 'secondhalfuntagged.txt':
         filename = mydir + "/" + ofilename
         print("processing %s \n" % filename)
         with open(filename,'r') as f:
             for row in f:
-                ##if it is a blank row, it will not need much processing:
+                #If the row has only whitespace, replace it with just a newline:
                 m = re.match("^\s*\n$", row)
                 if m:
                     s=s+"\n"
                 else:
+                    #Otherwise create a new tagged version of the row by stripping everything but the token and tag,
+                    #and a new untagged version by stripping everything but the token:
                     row = row.split("\t")
-                    ##print("Old row: ", row)
                     untagged = row[1] + "\n"
                     tagged = row[1] + "\t" + row[3] + "\n"
-                    ##this checks for sentence ends to make sure all are followed by breaks:
+                    #This checks for sentence ending tags to make sure all are followed by blank lines:
                     mt = re.match("^.*\$\.$", tagged)
                     mtt = re.match("^/.*SENT/.*$", tagged)
                     if mt or mtt:
                         s = s + tagged + "\n"
                     else:
                         s = s + tagged
-                    ##so does this:
+                    #This checks for sentence ending punctuation to do the same:
                     mu = re.match("^[\.?!:;]\s*$", untagged)
                     if mu:
                         u = u + untagged + "\n"
                     else:
                         u = u + untagged
                     print("New row in %s" % filename, ": ", row)
-                ##print m
-        ##here I write each new tagged file to its own new file:
+        #Here I write each newly processed tagged file to its own new file:
         newfilenamet = mydir + '/processed/' + ofilename.split(".")[0] + "_tagged.txt"
         print ("New tagged file named ", newfilenamet)
         with open("%s" % newfilenamet,'w') as nf:
@@ -45,19 +54,20 @@ for ofilename in secondhalf:
         print("File written to %s" % newfilenamet)
         nf.close()
         allt = allt + s
-        ##here I write each new untagged file to its own new file:
+        #Here I write each new untagged file to its own new file:
         newfilenameu = mydir + '/processed/' + ofilename.split(".")[0] + "_untagged.txt"
         with open("%s" % newfilenameu,'w') as nf:
             nf.write(u)
         print("New untagged file written to %s" % newfilenameu)
         nf.close()
         allu = allu + u
-##here I write every file from that half to a single file:
+#Here I concatenate every newly processed tagged file to a single file:
 endpath = mydir + '/secondhalftagged.txt'
 with open('%s' % endpath,'w') as f:
     f.write(allt)
     print("Entire tagged contents of folder written to file %s" % endpath)
     f.close()
+#Here I concatenate every new untagged file to a single file:
 endpath = mydir + '/secondhalfuntagged.txt'
 with open('%s' % endpath,'w') as f:
     f.write(allu)
